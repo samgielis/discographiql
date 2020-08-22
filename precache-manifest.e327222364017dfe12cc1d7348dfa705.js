003fab4992ecbc384457f4d26390585ab18c48ac
Sam Gielis <samgielis@gmail.com>
2020-08-22 17:02:37 +0200

    init gh-pages branch

---
+++ precache-manifest.e327222364017dfe12cc1d7348dfa705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503c7e883f0e0b4bb7a8fb9e5c08bdf1",
+    "url": "/discographiql/index.html"
+  },
+  {
+    "revision": "23ad49b7fee6cabdbc2b",
+    "url": "/discographiql/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "5d591ebb024f44e4de84",
+    "url": "/discographiql/static/js/2.33d1b705.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/discographiql/static/js/2.33d1b705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ad49b7fee6cabdbc2b",
+    "url": "/discographiql/static/js/main.3d658526.chunk.js"
+  },
+  {
+    "revision": "de4e96aac06dab6965e9",
+    "url": "/discographiql/static/js/runtime-main.9874f84f.js"
+  }
+]);
