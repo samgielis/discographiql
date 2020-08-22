57cea5bb0cd67d20a10b916eb2d8e9d48bffbb37
Sam Gielis <samgielis@gmail.com>
2020-08-22 22:46:11 +0200

    Improve artist page load by isolating discography fetch in separate component

--- src/components/ArtistProfile.tsx
+++ src/components/ArtistProfile.tsx
@@ -1,32 +1,18 @@
 import React from "react";
-import { ArtistWithDiscography, NamedNodeWithImage } from "../DataModel";
-import { Box, Heading, SimpleGrid, Image, Flex } from "@chakra-ui/core";
-import { defaultResponsiveMargin } from "../DefaultTheme";
-import ElegantImage from "./ElegantImage";
+import { Artist } from "../DataModel";
+import { Box } from "@chakra-ui/core";
 import ArtistHeader from "./ArtistHeader";
+import ArtistDiscography from "./ArtistDiscography";
 
 interface ArtistProfileProps {
-  artist: ArtistWithDiscography<NamedNodeWithImage>;
+  artist: Artist;
 }
 
 export default function ArtistProfile({ artist }: ArtistProfileProps) {
   return (
     <Box minH="100vh">
       <ArtistHeader artist={artist} />
-      <SimpleGrid
-        columns={{ base: 2, sm: 3, lg: 5 }}
-        spacing={defaultResponsiveMargin}
-        margin={defaultResponsiveMargin}
-      >
-        {artist.albums.map(({ id, name, image }) => (
-          <Box key={id} textAlign="center">
-            <ElegantImage src={image} alt={name} ratio={1} maxW="400px" />
-            <Heading size="md" mx={0} my={defaultResponsiveMargin}>
-              {name}
-            </Heading>
-          </Box>
-        ))}
-      </SimpleGrid>
+      <ArtistDiscography artist={artist} />
     </Box>
   );
 }
--- src/components/pages/ArtistPage.tsx
+++ src/components/pages/ArtistPage.tsx
@@ -1,26 +1,7 @@
 import React from "react";
-import { Spinner, Text, Box, IconButton } from "@chakra-ui/core";
-import { Artist, ArtistWithDiscography, NamedNode } from "../../DataModel";
-import { useQuery, gql } from "@apollo/client";
+import { Box, IconButton } from "@chakra-ui/core";
+import { Artist } from "../../DataModel";
 import ArtistProfile from "../ArtistProfile";
-
-const ARTISTPAGE = gql`
-  query ArtistPage($fullName: String!) {
-    queryArtists(byName: $fullName) {
-      id
-      name
-      albums {
-        id
-        name
-        image
-      }
-    }
-  }
-`;
-
-interface ArtistPageQueryResults {
-  queryArtists: ArtistWithDiscography<NamedNode>[];
-}
 
 interface ArtistPageProps {
   artist: Artist;
@@ -28,45 +9,14 @@ interface ArtistPageProps {
 }
 
 export function ArtistPage({ artist, onNavigateBack }: ArtistPageProps) {
-  const { loading, error, data } = useQuery<ArtistPageQueryResults>(
-    ARTISTPAGE,
-    {
-      variables: { fullName: artist.name },
-    }
+  return (
+    <Box>
+      <Box>
+        <NavigateBackButton onNavigateBack={onNavigateBack} />
+        <ArtistProfile artist={artist} />
+      </Box>
+    </Box>
   );
-
-  if (loading) {
-    return <Spinner />;
-  }
-
-  if (error) {
-    return (
-      <Text>
-        Something went wrong fetching this artist's profile. Please try again
-        later.
-      </Text>
-    );
-  }
-
-  if (data && data.queryArtists.length > 0) {
-    const artistWithDiscography = data.queryArtists.find((possibleMatch) => {
-      return possibleMatch.id === artist.id;
-    });
-
-    if (artistWithDiscography) {
-      let augmentedArtist = { ...artist, ...artistWithDiscography };
-      return (
-        <Box>
-          <Box>
-            <NavigateBackButton onNavigateBack={onNavigateBack} />
-            <ArtistProfile artist={augmentedArtist} />
-          </Box>
-        </Box>
-      );
-    }
-  }
-
-  return <Text>No search results.</Text>;
 }
 
 function NavigateBackButton({
