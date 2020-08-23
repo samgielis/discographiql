94b33879c2b9ba611fe953d1a8d548820c58c029
Sam Gielis <samgielis@gmail.com>
2020-08-23 11:11:03 +0200

    Add listen on spotify button to artist profile

--- src/components/ArtistHeader.tsx
+++ src/components/ArtistHeader.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Artist } from "../DataModel";
-import { Box, Flex, Heading, Image } from "@chakra-ui/core";
+import { Box, Stack, Heading, Image } from "@chakra-ui/core";
+import { FaSpotify } from "react-icons/fa";
+import LinkButton from "./LinkButton";
 
 interface ArtistHeaderProps {
   artist: Artist;
@@ -17,18 +19,28 @@ export default function ArtistHeader({ artist }: ArtistHeaderProps) {
         opacity={0.2}
         style={{ filter: "blur(3px)" }}
       />
-      <Flex
+      <Stack
         pos="absolute"
         alignItems="center"
         justify="center"
         w="100%"
         h="100%"
         top={0}
+        spacing={3}
       >
         <Heading size="2xl" color="white">
           {artist.name}
         </Heading>
-      </Flex>
+        <LinkButton
+          backgroundColor="green.400"
+          href={`https://open.spotify.com/artist/${artist.id}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Box as={FaSpotify} mr=".5em" fontSize="1.2em" />
+          Listen
+        </LinkButton>
+      </Stack>
     </Box>
   );
 }
