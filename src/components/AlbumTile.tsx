0b9efd0ac6e040fd600f5e6634feb90dd43b121f
Sam Gielis <samgielis@gmail.com>
2020-08-23 12:01:47 +0200

    Add open.spotify link to zlbum tiles

--- src/components/AlbumTile.tsx
+++ src/components/AlbumTile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Album } from "../DataModel";
-import { Box, Heading, BoxProps } from "@chakra-ui/core";
+import { Box, Heading, BoxProps, Link } from "@chakra-ui/core";
 import ElegantImage from "./ElegantImage";
 import { defaultResponsiveMargin } from "../DefaultTheme";
 
@@ -12,7 +12,13 @@ export function AlbumTile({ album, maxW }: AlbumTileProps) {
   const { name, image } = album;
   return (
     <Box textAlign="center" maxW={maxW}>
-      <ElegantImage src={image} alt={name} ratio={1} />
+      <Link
+        href={`https://open.spotify.com/album/${album.id}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <ElegantImage src={image} alt={name} ratio={1} />
+      </Link>
       <Heading size="md" mx={0} my={defaultResponsiveMargin}>
         {name}
       </Heading>
